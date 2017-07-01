1e5d7fab7467a87abc0ef86cd7321aba8db697d6
JasonJKi <ki.jasonj@gmail.com>
2017-06-30 21:13:21 -0400

    display test updated

--- DisplayTriggerSynchronizationTest/test.m
+++ DisplayTriggerSynchronizationTest/test.m
@@ -2,7 +2,7 @@ clear all
 dataDir= '..\DATA\';
 stimToEegCcaDir = '..\..\stim2eeg-cca\' 
 addpath([genpath('../liblsl-Matlab/') genpath(stimToEegCcaDir)])
-Filename= '..\DATA\180s_flash_30hz.xdf';
+Filename= '..\DATA\180s_flash_30hz_old30.xdf';
 [Streams,FileHeader] = load_xdf(Filename)
 
 for i =1:length(Streams)
@@ -13,31 +13,41 @@ end
 
 flashStreamInd=1
 StimulusPlayerInd=2
+for i = 1:length(Streams{flashStreamInd}.time_series)
+tmp1(i)=str2double(cell2mat(Streams{flashStreamInd}.time_series(i)));
+end
 tmp1=str2num(cell2mat(Streams{flashStreamInd}.time_series'));
+
 timeStamp1 = Streams{flashStreamInd}.time_stamps;
 tmp2=Streams{StimulusPlayerInd}.time_series(1,:)';
+tmp3=Streams{StimulusPlayerInd}.time_series(2,:)';
+
 timeStamp2 = Streams{StimulusPlayerInd}.time_stamps;
 dTimeStamp1 = diff(timeStamp1); %plot(dTimeStamp1)
-timeStampIndex = find(dTimeStamp1>.9)+1;
+timeStampIndex = find(dTimeStamp1>.9)+1;plot(dTimeStamp1)
 dTimeStampMean = mean(dTimeStamp1(timeStampIndex));
 dTimeStampStd = std(dTimeStamp1(timeStampIndex));
 flashStartTimeIndx1 = timeStamp1(timeStampIndex+1);
 m1 = mean(diff(flashStartTimeIndx1))
 s1 = std(diff(flashStartTimeIndx1))
 
-dTimeStampIndx2 = find(abs(diff(tmp2))>.03)+1;
-clf;hold on
-stem(dTimeStampIndx2,tmp2(dTimeStampIndx2),'r');
-plot(tmp2,'b')
-triggerIndx = dTimeStampIndx2(1:end);
-%triggerIndx = [1  ;dTimeStampIndx2(2:end)];
-frameTimeTriggerValue = tmp2(triggerIndx);%plot(frameTimeTriggerValue)
-frameTimeTriggerTimeStamp = timeStamp2(triggerIndx);
-std(diff(frameTimeTriggerTimeStamp))
-mean(diff(frameTimeTriggerTimeStamp))
+dTimeStampIndx2 = find((diff(tmp2) > median(diff(tmp2))-1 & diff(tmp2) < median(diff(tmp2))+1) | abs(max(diff(tmp2))))+1;
+dTimeStampIndx3 = find((diff(tmp3)) > .1);
 
 clf;hold on
-frameTimeTriggerTimeStamp_=frameTimeTriggerTimeStamp(61:30:end);
+dTimeStampIndx2=dTimeStampIndx2(7:end);
+stem(dTimeStampIndx2,tmp2(dTimeStampIndx2),'r');
+frameTimeTriggerTimeStamp=timeStamp2(dTimeStampIndx2)
+frameNumber = tmp3(dTimeStampIndx3)
+stem(dTimeStampIndx3,frameNumber,'k');
+frameTimeTriggerTimeStamp=timeStamp2(dTimeStampIndx3)
+
+%frameTimeTriggerTimeStamp=frameTimeTriggerTimeStamp__(:);
+%std(diff(frameTimeTriggerTimeStamp))
+%mean(diff(frameTimeTriggerTimeStamp))
+
+clf;hold on
+frameTimeTriggerTimeStamp_=frameTimeTriggerTimeStamp(60:30:end);
 plot(flashStartTimeIndx1,'.b')
 plot(frameTimeTriggerTimeStamp_,'.r')
 mean((flashStartTimeIndx1-frameTimeTriggerTimeStamp_))
@@ -104,3 +114,20 @@ stem(tmp1(startIndex:endIndex))
 % plot(diff(find(strcmp(Streams{1}.time_series,'5376')))')
 % 
 % Streams{2}.info.sample_count
+
+
+startIndx=find(tmp2(dTimeStampIndx2)==0)
+endIndx=find(tmp2(dTimeStampIndx2)==max(tmp2(dTimeStampIndx2)))
+indx =dTimeStampIndx2(startIndx(1):endIndx(end))
+stem(startIndx,tmp2(startIndx),'g')
+stem(endIndx,tmp2(endIndx),'k')
+
+plot(tmp2,'b')
+
+for i=1:length(endIndx)
+    indx=dTimeStampIndx2(startIndx(i):endIndx(i))
+    tmp2_(:,i)=tmp2(indx);
+    frameTimeTriggerTimeStamp__(:,i)=timeStamp2(indx)
+end
+frameTimeTriggerTimeStamp=timeStamp2(dTimeStampIndx2);
+
